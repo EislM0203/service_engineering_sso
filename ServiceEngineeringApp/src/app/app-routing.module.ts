b9b62a18ed4e04df0e6500efa941580db346d597
Viktoria Jechsmayr <6342890+vJechsmayr@users.noreply.github.com>
2023-01-17 22:46:26 +0100

    added Protected with working Routing

--- ServiceEngineeringApp/src/app/app-routing.module.ts
+++ ServiceEngineeringApp/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { KeycloakGuardService } from './keycloak-guard.service.';
-import {ProtectedComponent} from "./protected/protected.component";
-import {AppComponent} from "./app.component";
+import { ProtectedComponent } from "./protected/protected.component";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
 
   { path: 'protected', component: ProtectedComponent, canActivate: [KeycloakGuardService] },
-  { path: '', component: AppComponent,},
+  { path: 'home', component: HomeComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
 
 @NgModule({
--- ServiceEngineeringApp/src/app/header/header.component.ts
+++ ServiceEngineeringApp/src/app/header/header.component.ts
@@ -3,11 +3,11 @@ import { KeycloakService } from 'keycloak-angular';
 import { KeycloakProfile } from 'keycloak-js';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
-export class AppComponent implements OnInit {
+export class HeaderComponent implements OnInit {
   public isLoggedIn = false;
   public userProfile: KeycloakProfile | null = null;
 
