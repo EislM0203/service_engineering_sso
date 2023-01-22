80da1eafbf0953842b99269c755f66a0ba61af5c
Maximilian Sieber <101207002+zero048@users.noreply.github.com>
2023-01-22 17:12:57 +0100

    Löschen von Altlast

--- NodeBackend/config.js
+++
@@ -1,7 +0,0 @@
-configfile_name = 'config.js';
-
-exports.creds = {}; //SSO Creds heere
-
-exports.destroySessionUrl = ''; // Redirect after destroyed Session
-
-exports.database = {}; //Database Connection if needed
